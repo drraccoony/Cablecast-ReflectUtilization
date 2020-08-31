12725038d9c0ed39bfd51ea848675c1558e39174
Rico <drraccoony@gmail.com>
2020-08-30 20:12:53 -0500

    Some temp styling for fun

--- functions/func_getYesterdaysUsage.php
+++ functions/func_getYesterdaysUsage.php
@@ -1,15 +1,34 @@
-<?php
-  require_once  'db_connect.php';
-  $targetdate = date("Y-m-j", strtotime( '-1 days' ));
-  $sql = "SELECT * FROM customer_usage WHERE usageDate='$targetdate' ORDER BY `cost` DESC";
-  $result = $conn->query($sql);
+<?php include('../includes/head.php'); ?>
 
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Cost: " . $row["cost"]. " " . $row["data_use"]. "<br>";
+<?php $targetdate = date("Y-m-j", strtotime( '-1 days' )); ?>
+<div class="container">
+  <h1>functions\func_getYesterdaysUsage.php</h1>
+  <p>Pulls yesterdays usage, sorted by highest cost.</p>
+  <p><a href="../index.php">Return to Dashboard</a>.
+  <hr>
+  <?php
+    require_once  'db_connect.php';
+    $sql = "SELECT * FROM customer_usage WHERE usageDate='$targetdate' ORDER BY `cost` DESC";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      // output data of each row
+      print  '<table class="table table-striped">';
+      print  '<thead>';
+      print  '<tr>';
+      print    '<th>Customer Tag</th>';
+      print    '<th>Customer Name</th>';
+      print    '<th>Cost</th>';
+      print    '<th>Usage (Bytes)</th>';
+      print  '</tr>';
+      print '</thead>';
+      print '<tbody>';
+      while($row = $result->fetch_assoc()) {
+        echo "<tr><td>id: " . $row["id"]. "</td><td>???</td><td>$" . $row["cost"]. "</td><td>" . $row["data_use"]. " bytes</td></tr>";
+      }
+      print '</tbody>';
+      print '</table>';
+    } else {
+      echo "0 results";
     }
-  } else {
-    echo "0 results";
-  }
-?>
+  ?>
+</div>
