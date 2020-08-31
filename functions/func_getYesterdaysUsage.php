52623fa744c9d0a1bf1a0a09f80f4e8723108e89
Rico <drraccoony@gmail.com>
2020-08-30 20:01:36 -0500

    getYesterdaysUsage is functional

--- functions/func_getYesterdaysUsage.php
+++ functions/func_getYesterdaysUsage.php
@@ -1,11 +1,13 @@
 <?php
-  $sql = "SELECT id, firstname, lastname FROM MyGuests";
+  require_once  'db_connect.php';
+  $targetdate = date("Y-m-j", strtotime( '-1 days' ));
+  $sql = "SELECT * FROM customer_usage WHERE usageDate='$targetdate' ORDER BY `cost` DESC";
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      echo "id: " . $row["id"]. " - Cost: " . $row["cost"]. " " . $row["data_use"]. "<br>";
     }
   } else {
     echo "0 results";
