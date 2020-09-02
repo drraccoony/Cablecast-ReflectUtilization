d57917e0ad24f0004f95873296fda34e45dbaa40
Rico <drraccoony@gmail.com>
2020-09-02 10:59:44 -0500

    Updated Nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -15,29 +15,28 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li <?php if ($currentPage === 'index') {echo 'class="active"';} ?>><a href="index.php"><i class="fad fa-tachometer-alt-slowest"></i> Dashboard</a></li>
-        <li><a href="#"><i class="fad fa-file-chart-pie"></i> Reports</a></li>
-        <li><a href="#"><i class="fad fa-cogs"></i> Settings</a></li>
-        <!--<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fad fa-file-chart-pie"></i> Reports<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
+            <li><a href="functions\report_YesterdaysUsage.php">Yesterday</a></li>
+            <li><a href="#">Past Month</a></li>
+            <li><a href="#">Past Year</a></li>
+            <!--<li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="#">One more separated link</a></li>-->
           </ul>
-        </li>-->
+        </li>
+        <li><a href="#"><i class="fad fa-cogs"></i> Settings</a></li>
       </ul>
 
       <ul class="nav navbar-nav navbar-right">
         <form class="navbar-form navbar-left">
-          <div class="form-group">
+          <!--<div class="form-group">
             <input type="text" class="form-control" placeholder="Search">
           </div>
           <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+        </form>-->
         <!--<li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
